51db82e93588efeef1dc6bddc6f067ba2c923f73
Lars <larskuizenga@live.nl>
2019-12-09 09:53:53 +0100

    Page structure

---
+++ includes/topinclude.php
@@ -0,0 +1,10 @@
+<!DOCTYPE HTML>
+<html lang="nl">
+	<head>
+	
+	</head>
+	<body>
+		<div class="container">
+			<div class="navbar">
+				
+			</div>
---
+++ index.php
@@ -0,0 +1,7 @@
+<?php include "includes/topinclude.php" ?>
+
+	<div class="content">
+		
+	</div>
+
+<?php include "includes/bottominclude.php" ?>
