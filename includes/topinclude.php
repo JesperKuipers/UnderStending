407e18084c1e63ae4322a5d321e6968b064f8e9c
Lars <larskuizenga@live.nl>
2019-12-09 11:47:59 +0100

    Navbar first setup

--- includes/topinclude.php
+++ includes/topinclude.php
@@ -1,10 +1,17 @@
 <!DOCTYPE HTML>
 <html lang="nl">
 	<head>
-	
+		<meta charset="utf-8">
+		<link rel="stylesheet" type="text/css" href="css/style.css">
 	</head>
 	<body>
 		<div class="container">
 			<div class="navbar">
-				
+				<img src="imgs/understending-logo.png" class="logo-image">
+				<div class="search-bar">
+					<form method="get" action="search.php">
+						<input type="text" name="q" placeholder="Zoeken op UnderStending" class="search-bar-bar">
+						<input type="image" src="imgs/search-icon.png" border="0" alt="Submit" class="search-icon"/>
+					</form>
+				</div>
 			</div>
