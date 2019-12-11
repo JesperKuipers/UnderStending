b2746eda8f08ca11d656c49e6a559670ff1410f3
Lars <larskuizenga@live.nl>
2019-12-11 15:46:39 +0100

    Function compiler base

--- function-compiler.php
+++ function-compiler.php
@@ -1,19 +1,29 @@
 <?php
 	echo "<h2>Functions</h2>";
 	function dirToArray($dir) {
-		$result = array();
+		//$result = array();
 
 		$cdir = scandir($dir);
 		foreach ($cdir as $key => $value) {
 			if (!in_array($value,array(".",".."))) {
 				if (is_dir($dir . DIRECTORY_SEPARATOR . $value)) {
-					$result[$value] = dirToArray($dir . DIRECTORY_SEPARATOR . $value);
+					//$result[$value] =
+					dirToArray($dir . DIRECTORY_SEPARATOR . $value);
 				} else {
-					$result[] = $value;
+					//$result[] = $value;
+					if($value !== "functions.php") {
+						$path = $dir . DIRECTORY_SEPARATOR . $value;
+						$content = file_get_contents(, FALSE, NULL, 5);
+						$content = str_replace("?>", "", $content);
+						echo "file content ($value): <br>";
+						file_put_contents("core/functions.php", $content);
+					}
+					var_dump($content);
+					echo "<br>";
 				}
 			}
 		}
-		return $result;
+		//return $result;
 	}
 	echo '<pre>' . print_r(dirToArray("functions"), TRUE) . '</pre>';
 ?>
