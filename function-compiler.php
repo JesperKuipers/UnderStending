51218f0266a3fbc3a7b47654f4a8d7d10e737668
Sydney Minnaar <sydneyminnaar2000@hotmail.com>
2019-12-12 17:27:25 +0100

    Improved controlability compiler

--- function-compiler.php
+++ function-compiler.php
@@ -1,7 +1,11 @@
 <?php
+	$compileDirectory = "core";
+	$targetFile = "core/functions.php";
+	$ignoredFiles = ["functions.php", "test.php"];
+
 	echo "<h2>Functions</h2>";
 	//Function compiler function
-	function functionCompiler($dir) {
+	function functionCompiler($dir, $target, $ignored) {
 		//Scan the given directory
 		$cdir = scandir($dir);
 		//Loop through all files and directories in given directory
@@ -11,11 +15,11 @@
 				//Check if current file is a directory
 				if (is_dir($dir . DIRECTORY_SEPARATOR . $filename)) {
 					//Call back to function to loop through nested directory
-					functionCompiler($dir . DIRECTORY_SEPARATOR . $filename);
+					functionCompiler($dir . DIRECTORY_SEPARATOR . $filename, $target, $ignored);
 				//Check if current file is a file
 				} else {
 					//Exclude functions.php
-					if($filename !== "functions.php") {
+					if(!in_array($filename, $ignored)) {
 						//Get the path to this file
 						$path = $dir . DIRECTORY_SEPARATOR . $filename;
 						//Get the contents of this file without the php opening tags
@@ -23,12 +27,12 @@
 						//Take the php closing tags out of the files
 						$content = str_replace("?>", "", $content);
 						//Get the contents of the current functions.php file
-						$current = file_get_contents("core/functions.php");
+						$current = file_get_contents($target);
 						//Add the contents of the new current file to functions.php
 						$current .= $content;
 						
 						//Put the new content back into functions.php
-						file_put_contents("core/functions.php", $current);
+						file_put_contents($target, $current);
 						//Show which file was added
 						echo "The function file: ($path) has been added<br>";
 					}
@@ -36,14 +40,15 @@
 			}
 		}
 	}
-	//Empty the current functions.php and add opening php tags at the start
-	file_put_contents("core/functions.php", "<?php");
-	//Call to the Function compiler function
-	functionCompiler("core");
-	//Get the contents of the current functions.php file
-	$tags = file_get_contents("core/functions.php");
-	//Add closing tags to the string of the current functions.php
+		
+	//Empty the target file and add opening php tags at the start
+	file_put_contents($targetFile, "<?php");
+	//Call to the compiler
+	functionCompiler($compileDirectory, $targetFile, $ignoredFiles);
+	//Get the contents of the target file
+	$tags = file_get_contents($targetFile);
+	//Add closing tags to the string of the target file
 	$tags .= "?>";
-	//Put the new content with the php closing tags back into the functions.php
-	file_put_contents("core/functions.php", $tags);
+	//Put the new content with the php closing tags back into the target file
+	file_put_contents($targetFile, $tags);
 ?>
