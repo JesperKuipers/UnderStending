c20aca4e610c61e34419eb5d5fa29f008bac6312
Lars <larskuizenga@live.nl>
2019-12-13 10:22:29 +0100

    Login.php conflict

--- login.php
+++ login.php
@@ -1,25 +1,28 @@
 <?php include "includes/topinclude.php"; ?>
+
 <?php
     session_start();
     include("loginBackend.php");
 ?>
-<div class="content">
-    <div class="content-block">
-        <h1>Login</h1>
-    </div>
-</div>
-<form action="login.php" method="post">
-    <table>
-        <tr>
-            <td>email:</td>
-            <td><input type="text" name="email" /></td>
-        </tr>
-        <tr>
-            <td>password:</td>
-            <td><input type="password" name="password" /></td>
-        </tr>
-    </table>
-    <input type="submit" name="submit" />
-</form>
+	<div class="content">
+		<div class="content-block">
+			<h1>Login</h1>
+		</div>
+		<div class="formcontainer">
+			<form action="login.php" method="post">
+					<div class="form-block">
+						<div class="form-block-label">email:</div>
+						<div class="form-block-field"><input type="text" name="email" /></div>
+					<div>
+					<div class="form-field">
+						<div class="form-block-label">password:</div>
+						<div class="form-block-field"><input type="password" name="password" /></div>
+					</div>
+				</table>
+				<input type="submit" name="submit" />
+			</form>
+		</div>
+	</div>
+	
 
 <?php include "includes/bottominclude.php" ?>
