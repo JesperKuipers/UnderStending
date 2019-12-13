2e405ee6d5b51f1947e1f7cf61ac0ec073c39536
Marvin Hoffenkamp <56029167+MarvinHoffenkamp@users.noreply.github.com>
2019-12-13 10:25:34 +0100

    Update loginBackend.php

--- loginBackend.php
+++ loginBackend.php
@@ -2,16 +2,17 @@
     $email = filter_input(INPUT_POST, "email", FILTER_SANITIZE_EMAIL);
     $password = filter_input(INPUT_POST, "password", FILTER_SANITIZE_SPECIAL_CHARS);
     
-    $query = "SELECT password, name FROM user WHERE email = ?;";
+    $query = "SELECT password, name, userID FROM user WHERE email = ?;";
     if($statement = mysqli_prepare($conn, $query)) {
         mysqli_stmt_bind_param($statement, "s", $email);
         if (mysqli_stmt_execute($statement)) {
-            mysqli_stmt_bind_result($statement, $userPassword, $name);
+            mysqli_stmt_bind_result($statement, $userPassword, $name, $userID);
             mysqli_stmt_fetch($statement);
             if(password_verify($password, $userPassword)){
                 session_start();
                 $_SESSION['name'] = $name;
-                echo "You have been logged in as $name";
+                $_SESSION['userID'] = $userID;
+                echo "You have been logged in as $name, ID: $userID";
             } else {
                 echo "Email and/or password incorrect.";
             }
