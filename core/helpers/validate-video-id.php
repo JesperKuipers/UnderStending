3f5116901689df856581729af582e04cd8259746
Lars <larskuizenga@live.nl>
2020-01-07 13:58:51 +0100

    Delete/Update video pagina's

--- core/helpers/validate-video-id.php
+++ core/helpers/validate-video-id.php
@@ -1,9 +1,19 @@
 <?php
 function getVideoID() {
-    $videoID = filter_input(INPUT_GET, 'v', FILTER_VALIDATE_INT);
-    if (!$videoID) {
-        return false;
-    } else {
-        return $videoID;
-    }
+	if(isset($_GET["v"])) {
+		$videoID = filter_input(INPUT_GET, 'v', FILTER_VALIDATE_INT);
+		if (!$videoID) {
+			return false;
+		} else {
+			return $videoID;
+		}
+	}
+	else if(isset($_GET["id"])) {
+		$videoID = filter_input(INPUT_GET, 'id', FILTER_VALIDATE_INT);
+		if (!$videoID) {
+			return false;
+		} else {
+			return $videoID;
+		}
+	}
 }
--- manage-tag.php
+++ manage-tag.php
@@ -1,19 +1,33 @@
 <?php include "includes/topinclude.php" ?>
 
+<?php 
+	$userID = $_SESSION["userID"];
+	
+	
+	if(!empty($_POST["submit"])) {
+		if(isset($_POST["delete"])) {
+			$tagID = $_POST["tagid"];
+			RemoveTag($tagID, $userID);
+			$confirm = "Tag verwijdert";
+		} 
+	}
+?>
 	<div class="content">
 		<div class="content-block">
-			<h2>Manage videos</h2>
+			<?php if(isset($confirm)) { echo '<div class="form-confirm-block">' . $confirm . '</div>'; } ?>
+			<h2>Manage tags</h2>
 			<div class="block-manage-container">
 				<div class="block-add"><a href="add-tag.php">&#10010; Tag toevoegen </a></div>
-				<!-- PHP get all videos -->
 				<?php
-					for($i=1; $i<8; $i++) {
-						echo "<div class='video-title'>
-								<a href='delete-tag.php?id=" . $i . "' class='block-title-delete'>&#10006;</a> 
-								<a href='edit-tag.php?id=" . $i . "' class='block-title-edit'>&#9998;</a> 
-								<a href='tag.php?id=" . $i . "' class='block-title-video'>Naam van de tag</a>
-							</div>";
-					} 
+					//$tags = GetTags(500);
+					if(!empty($tags)) {
+						foreach($tags as $tag) {
+							echo "<div class='tag-title'>
+									<a href='delete-tag.php?id=" . $tag->tagId . "' class='block-title-delete'>&#10006;</a> 
+									<a href='tag.php?id=" . $tag->tagId . "' class='block-title-video'>" . $tag->name . "</a>
+								</div>";
+						}
+					}
 				?>
 			</div>
 		</div>
