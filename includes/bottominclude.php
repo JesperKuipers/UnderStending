4f6b8a7f51d83e32d468e8ca0252b816a461cf8d
Marvin Hoffenkamp <56029167+MarvinHoffenkamp@users.noreply.github.com>
2020-01-17 12:07:49 +0100

    Update bottominclude.php

--- includes/bottominclude.php
+++ includes/bottominclude.php
@@ -5,18 +5,17 @@
 					<img src="imgs/nhl-made-by-students.png" class="nhl-made-by-students">
 				</div>
 				<div class="footer-block">
-				<?php
-					if ($_SESSION['language'] == 'en') {
-						echo "<a href='?language=nl'>Zet taal naar Nederlands</a>";
-					} else {
-						echo "<a href='?language=en'>Set language to English</a>";
-					}
-					
-					echo $_SESSION['language'];
-				?>
+				
 				</div>
 				<div class="footer-block">
-				
+				<?php
+					if ($_SESSION['language'] == 'en') {
+						echo "<a href='?language=nl' style='color:white;'>Zet taal naar Nederlands</a>";
+					} else {
+						echo "<a href='?language=en' style='color:white;'>Set language to English</a>";
+					}
+					
+				?>
 				</div>
 			</div>
 		</div>
@@ -26,18 +25,17 @@
 					<img src="imgs/nhl-made-by-students.png" class="nhl-made-by-students">
 				</div>
 				<div class="footer-block">
-				<?php
-					if ($_SESSION['language'] == 'en') {
-						echo "<a href='?language=nl'>Zet taal naar Nederlands</a>";
-					} else {
-						echo "<a href='?language=en'>Set language to English</a>";
-					}
-					
-					echo $_SESSION['language'];
-				?>
+				
 				</div>
 				<div class="footer-block">
-				
+				<?php
+					if ($_SESSION['language'] == 'en') {
+						echo "<a href='?language=nl' style='color:white;'>Zet taal naar Nederlands</a>";
+					} else {
+						echo "<a href='?language=en' style='color:white;'>Set language to English</a>";
+					}
+					
+				?>
 				</div>
 			</div>
 		</div>
