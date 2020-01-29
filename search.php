003aa1e3efee3d51bd1666d944d682079b6d3409
Lars <larskuizenga@live.nl>
2020-01-29 11:01:15 +0100

    Added empty check on search and homepage tags

--- search.php
+++ search.php
@@ -12,57 +12,64 @@
 		<h2>Resultaten voor "<?php echo $query; ?>"</h2>
 		<?php } ?>
 		<div class="blocks">
-			<?php 
-			foreach($results as $result) {
-				if(isset($result[0]->videoId)) {
-					foreach($result as $video) {
-						$videoID = $video->videoId;
-						$title = $video->title;
-						$thumbnail = $video->ThumbnailUrl();
-						
-						echo "<a href='video.php?v=" . $videoID . "'>";
-							echo "<div class='block'>";
-								echo "<div class='block-naam video-naam'>";
-									echo $title;
+			<?php if(!empty($results)) {
+				foreach($results as $result) {
+					if(isset($result[0]->videoId)) {
+						foreach($result as $video) {
+							$videoID = $video->videoId;
+							$title = $video->title;
+							$thumbnail = $video->ThumbnailUrl();
+							
+							echo "<a href='video.php?v=" . $videoID . "'>";
+								echo "<div class='block'>";
+									echo "<div class='block-naam video-naam'>";
+										echo $title;
+									echo "</div>";
+									echo "<img src='" . $thumbnail . "' />";
 								echo "</div>";
-								echo "<img src='" . $thumbnail . "' />";
-							echo "</div>";
-						echo "</a>";
+							echo "</a>";
+						}
 					}
-				}
-				else if(isset($result[0]->tagId)) {
-					foreach($result as $tag) {
-						$tagID = $tag->tagId;
-						$name = $tag->name;
-						$thumbnail = $tag->thumbnailId . "." . $tag->thumbnailExtension;
-						
-						echo "<a href='tag.php?v=" . $tagID . "'>";
-							echo "<div class='block'>";
-								echo "<div class='block-naam tag-naam'>";
-									echo $name;
+					else if(isset($result[0]->tagId)) {
+						foreach($result as $tag) {
+							$tagID = $tag->tagId;
+							$name = $tag->name;
+							$thumbnail = $tag->thumbnailId . "." . $tag->thumbnailExtension;
+							
+							echo "<a href='tag.php?v=" . $tagID . "'>";
+								echo "<div class='block'>";
+									echo "<div class='block-naam tag-naam'>";
+										echo $name;
+									echo "</div>";
+									echo "<img src='imgs/thumbnails/" . $thumbnail . "' />";
 								echo "</div>";
-								echo "<img src='imgs/thumbnails/" . $thumbnail . "' />";
-							echo "</div>";
-						echo "</a>";
+							echo "</a>";
+						}
 					}
-				}
-				else if(isset($result[0]->playlistId)) {
-					foreach($result as $playlist) {
-						$playlistID = $playlist->playlistId;
-						$name = $playlist->name;
-						$thumbnail = $playlist->thumbnailId . "." . $playlist->thumbnailExtension;
-						
-						echo "<a href='playlist.php?v=" . $playlistID . "'>";
-							echo "<div class='block'>";
-								echo "<div class='block-naam playlist-naam'>";
-									echo $name;
+					else if(isset($result[0]->playlistId)) {
+						foreach($result as $playlist) {
+							$playlistID = $playlist->playlistId;
+							$name = $playlist->name;
+							$thumbnail = $playlist->thumbnailId . "." . $playlist->thumbnailExtension;
+							
+							echo "<a href='playlist.php?v=" . $playlistID . "'>";
+								echo "<div class='block'>";
+									echo "<div class='block-naam playlist-naam'>";
+										echo $name;
+									echo "</div>";
+									echo "<img src='imgs/thumbnails/" . $thumbnail . "' />";
 								echo "</div>";
-								echo "<img src='imgs/thumbnails/" . $thumbnail . "' />";
-							echo "</div>";
-						echo "</a>";
+							echo "</a>";
+						}
 					}
+				} 
+			} else {
+				if ($_SESSION['language'] == "en") {
+					echo "<p>No results for your query</p>";
+				} else {
+					echo "<p>Geen resultaten voor uw zoekopdracht</p>";
 				}
-			} 
+			}
 			?>
 		</div>
 	</div>
